3b593eab748195a608cdaee668bb893dc66dbbc4
nichujie <ncj19991213@126.com>
2022-12-03 22:24:07 +0100

    basic router

--- src/main.jsx
+++ src/main.jsx
@@ -16,7 +16,31 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
-        element: <Home />,
+        element: <Home />, // TODO: home page
+      },
+      {
+        path: "login",
+        element: <div>Login</div>, // TODO
+      },
+      {
+        path: "register",
+        element: <div>Register</div>, // TODO
+      },
+      {
+        path: "profile",
+        element: <div>Profile</div>, // TODO: note we need route guard
+      },
+      {
+        path: "random",
+        element: <div>Random</div>, // TODO: get a random cocktail when loading, click to get another
+      },
+      {
+        path: "search",
+        element: <div>Search</div>, // TODO: search results page; read query from url param; multiple filters
+      },
+      {
+        path: "detail/:cocktailId",
+        element: <div>Cocktail Detail</div>,
       },
     ],
   },
