55316244bac97372229c63ae56f068a95af284a0
10RE <lorenlin@umich.edu>
2023-02-22 21:25:27 -0500

    remove prev code from database

--- src/models/database.jsx
+++ src/models/database.jsx
@@ -1,44 +1,6 @@
 import { doc, getDoc, setDoc } from "firebase/firestore";
 
 import database from "@/clients/firebase";
-/**
- * User field (todo)
- *
- */
-// const DBContext = createContext({
-//   fetchedData: undefined,
-//   pullData: () => {},
-//   setData: () => {},
-// });
-
-// const DBContextProvider = ({ children }) => {
-//   const [fetchedData, setFetchedData] = useState();
-
-//   const pullData = async (collection, id) => {
-//     console.log("Pulling data");
-//     const docRef = doc(database, collection, id);
-//     const docSnap = await getDoc(docRef);
-//     console.log(docSnap.data());
-//     setFetchedData(docSnap.data());
-//     return docSnap.data();
-//   };
-
-//   const setData = async (collection, id, data) => {
-//     const docRef = doc(database, collection, id);
-//     await setDoc(docRef, data);
-//   };
-
-//   const contextValue = useMemo(
-//     () => ({
-//       fetchedData,
-//       pullData,
-//       setData,
-//     }),
-//     [fetchedData, pullData]
-//   );
-
-//   return <DBContext.Provider value={contextValue}>{children}</DBContext.Provider>;
-// };
 
 class Database {
   async pullData(collection, id) {
@@ -48,6 +10,7 @@ class Database {
     console.log(docSnap.data());
     return new Promise((resolve, reject) => {
       resolve(docSnap.data());
+      reject("Error");
     });
   }
 
