6262471bd25ded3f64b6fee207d328e426c48f4b
10RE <lorenlin@umich.edu>
2023-03-09 11:30:20 -0500

    fix, remove more profile

--- src/models/index.js
+++ src/models/index.js
@@ -2,10 +2,8 @@ import { useContext } from "react";
 
 import { AuthContext } from "./auth";
 import { JobContext } from "./job";
-import { ProfileContext } from "./profile";
 
 const useAuth = () => useContext(AuthContext);
-const useProfile = () => useContext(ProfileContext);
 const useJob = () => useContext(JobContext);
 
-export { useAuth, useJob, useProfile };
+export { useAuth, useJob };
