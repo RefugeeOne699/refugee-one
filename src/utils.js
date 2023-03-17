5b4c5457bd3bf5f8c4c5183858dd9e69bf4fd734
makersmelx <makersmelx@gmail.com>
2023-03-16 21:23:05 -0400

    feat: transfer between days of week with number in Job.shift.daysOfWeek

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,4 @@
+import { DAYS_OF_WEEK } from "./constants";
 export function debounce(fn, t) {
   let timer = null;
   return function () {
@@ -6,4 +7,28 @@ export function debounce(fn, t) {
       fn.apply(this, arguments);
     }, t);
   };
+}
+
+/**
+ * Convert a number 0-127 to a set of days of week
+ * @param {Number} num
+ * @returns a list of days of week
+ */
+export function numberToDaysOfWeek(num) {
+  let daysOfWeek = [];
+  for (let i = 6; i >= 0; i--) {
+    if (parseInt(num / 2 ** i) > 0) {
+      daysOfWeek.push(DAYS_OF_WEEK[i]);
+    }
+    num = num % 2 ** i;
+  }
+  return daysOfWeek.reverse();
+}
+
+/**
+ * Convert a set of days of week to a number 0-127
+ * @param {String[]} days
+ */
+export function daysOfWeekToNumber(days) {
+  return days.reduce((acc, day) => acc + 2 ** DAYS_OF_WEEK.indexOf(day), 0);
 }
