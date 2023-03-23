20cda674c02237c9090954382574538fb8facc5f
10RE <lorenlin@umich.edu>
2023-03-22 20:51:03 -0400

    remove comments

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -27,24 +27,5 @@ export default function Profile() {
       </div>
       <Outlet />
     </div>
-
-    // <div className="flex flex-col justify-center items-center">
-    //   <label className="label" htmlFor="name">
-    //     <span className="label-text">Full Name</span>
-    //   </label>
-    //   <p className="input w-full max-w-xs input-bordered mb-4">{auth.user?.name}</p>
-    //   <label className="label" htmlFor="email">
-    //     <span className="label-text">Email</span>
-    //   </label>
-    //   <p className="input w-full max-w-xs input-bordered mb-4">{auth.user?.email}</p>
-    //   <label className="label" htmlFor="phone">
-    //     <span className="label-text">Phone</span>
-    //   </label>
-    //   <p className="input w-full max-w-xs input-bordered mb-4">{auth.user?.phone}</p>
-    //   <label className="label" htmlFor="role">
-    //     <span className="label-text">Role</span>
-    //   </label>
-    //   <p className="input w-full max-w-xs input-bordered mb-4">{auth.user?.role}</p>
-    // </div>
   );
 }
