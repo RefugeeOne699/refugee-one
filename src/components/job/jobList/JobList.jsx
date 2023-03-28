d7043f7e3fa91f8b5c10a1af42380cd832e46b41
Tianchi Fu <tianchi@umich.edu>
2023-03-28 11:55:12 -0400

    fixed useMemo bug

--- src/components/job/jobList/JobList.jsx
+++ src/components/job/jobList/JobList.jsx
@@ -42,7 +42,7 @@ export default function JobList() {
 
   const searchRef = useRef();
 
-  const { register, reset, handleSubmit } = useForm({
+  const { register, reset, getValues, handleSubmit } = useForm({
     defaultValues: emptyFilter,
   });
 
@@ -52,97 +52,9 @@ export default function JobList() {
     setShowFilter(!showFilter);
   };
 
-  useEffect(() => {
-    if (auth.user) {
-      (async () => {
-        await run();
-      })();
-    }
-  }, []);
-
-  // useEffect for filter and search
-  useEffect(() => {
-    const filteredResult = getSearchAndFilterResult(data, search, filter);
-    setFilteredJobs(filteredResult);
-  }, [filter, search, data]);
-
-  const jobs = useMemo(() => {
-    return filteredJobs
-      ? filteredJobs.map((job) => {
-          return (
-            <li className="flex flex-row w-full mt-5 " key={job.id}>
-              <div
-                className={`card card-compact w-full rounded-xl border-slate-400 border-4 ${
-                  job.id && job.id == jobId ? "active" : ""
-                }`}
-              >
-                <Link to={job.id} className="w-full flex flex-row justify-between">
-                  <div className="card-body basis-9/12 flex-none">
-                    <div className="card-title text-xl">{job.title}</div>
-                    <div className="flex flex-row flex-none">
-                      <div className="flex flex-row flex-wrap flex-auto">
-                        <p className="truncate w-1/2">{job.company.name}</p>
-                        <p className="truncate w-1/2">{`${job.location}`}</p>
-                        <p className="truncate w-1/2">
-                          {job.benefit.hasMedical
-                            ? "Medical Benefits"
-                            : "No Medical Benefits"}
-                        </p>
-                        <p className="truncate w-1/2">
-                          Minimum Pay: {job.wage.min} {job.wage.type}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="card-actions basis-1/12 items-center justify-center">
-                    <JobSave jobId={job.id} mode={"list"} />
-                  </div>
-                </Link>
-              </div>
-            </li>
-          );
-        })
-      : "Loading";
-  }, [filteredJobs, jobId]);
-
-  return (
-    <div className="relative flex flex-col h-screen  bg-yellow-100">
-      {/* Search bar and filter icon */}
-      <div className="flex flex-row items-center justify-between h-16 p-3 bg-blue-100">
-        <div className="relative text-gray-600">
-          <input
-            className="w-64 border-2 border-gray-300 bg-white h-10 px-5 pr-10 rounded-lg text-sm focus:outline-none"
-            type="search"
-            name="search"
-            placeholder="Search"
-            ref={searchRef}
-            onKeyDown={(e) => {
-              if (e.key === "Enter") setSearch(searchRef.current.value);
-            }}
-          />
-          <button
-            className="absolute right-0 top-1.5  mr-4 "
-            onClick={() => {
-              setSearch(searchRef.current.value);
-            }}
-          >
-            <SearchIcon />
-          </button>
-        </div>
-        <button
-          className={
-            _.isEqual(filter, emptyFilter)
-              ? "btn btn-circle btn-ghost"
-              : "btn btn-circle btn-primary"
-          }
-          onClick={() => setShowFilter(!showFilter)}
-        >
-          <FilterAltIcon fontSize="large" />
-        </button>
-      </div>
-
-      {/* Filter Panel UI */}
-      {showFilter ? (
+  const filterUI = useMemo(() => {
+    if (showFilter) {
+      return (
         <div className="absolute z-10 top-0 left-0 w-full h-full bg-white overflow-scroll">
           {/* Top Bar */}
           <div className="sticky top-0 left-0 flex flex-row justify-between items-center w-full h-16 p-3 bg-slate-200">
@@ -315,7 +227,103 @@ export default function JobList() {
             </form>
           </div>
         </div>
-      ) : null}
+      );
+    } else {
+      return null;
+    }
+  }, [showFilter, getValues()]);
+
+  useEffect(() => {
+    if (auth.user) {
+      (async () => {
+        await run();
+      })();
+    }
+  }, []);
+
+  // useEffect for filter and search
+  useEffect(() => {
+    const filteredResult = getSearchAndFilterResult(data, search, filter);
+    setFilteredJobs(filteredResult);
+  }, [filter, search, data]);
+
+  const jobs = useMemo(() => {
+    return filteredJobs
+      ? filteredJobs.map((job) => {
+          return (
+            <li className="flex flex-row w-full mt-5 " key={job.id}>
+              <div
+                className={`card card-compact w-full rounded-xl border-slate-400 border-4 ${
+                  job.id && job.id == jobId ? "active" : ""
+                }`}
+              >
+                <Link to={job.id} className="w-full flex flex-row justify-between">
+                  <div className="card-body basis-9/12 flex-none">
+                    <div className="card-title text-xl">{job.title}</div>
+                    <div className="flex flex-row flex-none">
+                      <div className="flex flex-row flex-wrap flex-auto">
+                        <p className="truncate w-1/2">{job.company.name}</p>
+                        <p className="truncate w-1/2">{`${job.location}`}</p>
+                        <p className="truncate w-1/2">
+                          {job.benefit.hasMedical
+                            ? "Medical Benefits"
+                            : "No Medical Benefits"}
+                        </p>
+                        <p className="truncate w-1/2">
+                          Minimum Pay: {job.wage.min} {job.wage.type}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="card-actions basis-1/12 items-center justify-center">
+                    <JobSave jobId={job.id} mode={"list"} />
+                  </div>
+                </Link>
+              </div>
+            </li>
+          );
+        })
+      : "Loading";
+  }, [filteredJobs, jobId]);
+
+  return (
+    <div className="relative flex flex-col h-screen  bg-yellow-100">
+      {/* Search bar and filter icon */}
+      <div className="flex flex-row items-center justify-between h-16 p-3 bg-blue-100">
+        <div className="relative text-gray-600">
+          <input
+            className="w-64 border-2 border-gray-300 bg-white h-10 px-5 pr-10 rounded-lg text-sm focus:outline-none"
+            type="search"
+            name="search"
+            placeholder="Search"
+            ref={searchRef}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") setSearch(searchRef.current.value);
+            }}
+          />
+          <button
+            className="absolute right-0 top-1.5  mr-4 "
+            onClick={() => {
+              setSearch(searchRef.current.value);
+            }}
+          >
+            <SearchIcon />
+          </button>
+        </div>
+        <button
+          className={
+            _.isEqual(filter, emptyFilter)
+              ? "btn btn-circle btn-ghost"
+              : "btn btn-circle btn-primary"
+          }
+          onClick={() => setShowFilter(!showFilter)}
+        >
+          <FilterAltIcon fontSize="large" />
+        </button>
+      </div>
+
+      {/* Filter Panel UI */}
+      {filterUI}
 
       {/* Job List UI */}
       <ul className="menu w-full">{jobs}</ul>
