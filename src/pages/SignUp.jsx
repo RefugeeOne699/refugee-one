c76e2be2d7ffaac5fece5b6b049851d23f08be54
nehakumari2307 <35798530+nehakumari2307@users.noreply.github.com>
2023-03-29 19:58:37 -0500

    Update SignUp UI (#32)
    * Update SignUp UI
    
    Clean up SignUp page according to the wireframe
    
    * Update SignUp page
    
    Add toast message and temp hardcode for company field

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -2,12 +2,11 @@ import { useRequest } from "ahooks";
 // import { sendEmailVerification } from "firebase/auth";
 import { doc } from "firebase/firestore";
 import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 // import { auth as firebaseAuth } from "@/clients/firebase";
-// fixme: temp hard code company
 import database from "@/clients/firebase";
-import { ROLES } from "@/constants";
 import { useAuth } from "@/models";
 
 export default function SignUp() {
@@ -17,7 +16,7 @@ export default function SignUp() {
   // follow this function for a better practice with interactive button
 
   const { run: signUp, loading: signUpLoading } = useRequest(
-    // fixme: temp hard code company
+    // No harding for company required as it will be entered by user when registering for first time
 
     async (data) =>
       auth.signUp({
@@ -37,10 +36,12 @@ export default function SignUp() {
         //     );
         //   });
         // }
+        toast.success("Registration Successful");
         navigate("/");
       },
       onError: (error) => {
         //todo: handle error
+        toast.error("Registration Failed");
         console.error(error);
       },
     }
@@ -52,73 +53,70 @@ export default function SignUp() {
 
   return (
     <div className="flex flex-col justify-center items-center">
-      <p className="text-xl">Note: this is a temporary sign up place</p>
-      <form onSubmit={handleSubmit(signUp)}>
-        <label className="label" htmlFor="name">
-          <span className="label-text">Full Name</span>
-        </label>
-        <input
-          {...register("name", { required: true })}
-          type="text"
-          className="input w-full max-w-xs input-bordered mb-4"
-        />
-        <label className="label" htmlFor="compnay">
-          <span className="label-text">Company</span>
-        </label>
-        {/* fixme: a selector for companies, currently not required and has a default value */}
-        <input
-          {...register("company")}
-          type="text"
-          className="input w-full max-w-xs input-bordered mb-4"
-          disabled
-        />
-        <label className="label" htmlFor="email">
-          <span className="label-text">Email</span>
-        </label>
-        <input
-          {...register("email", { required: true })}
-          type="email"
-          className="input w-full max-w-xs input-bordered mb-4"
-        />
-        <label className="label" htmlFor="password">
-          <span className="label-text">Password</span>
-        </label>
-        <input
-          {...register("password", { required: true })}
-          type="password"
-          className="input w-full max-w-xs input-bordered mb-4"
-        />
-        <label className="label" htmlFor="phone">
-          <span className="label-text">Phone</span>
-        </label>
-        <input
-          {...register("phone", { required: true })}
-          type="number"
-          className="input w-full max-w-xs input-bordered mb-4"
-        />
-        <label className="label" htmlFor="role">
-          <span className="label-text">Role</span>
-        </label>
-        <select
-          className="input w-full max-w-xs input-bordered mb-4"
-          {...register("role", { required: true })}
-        >
-          {Object.values(ROLES).map((type, index) => {
-            return (
-              <option value={type} key={index}>
-                {type}
-              </option>
-            );
-          })}
-        </select>
-        <button
-          type="submit"
-          className={`btn btn-primary ${signUpLoading ? "loading" : ""}`}
-          disabled={signUpLoading}
-        >
-          {signUpLoading ? "Loading" : "Sign Up"}
-        </button>
-      </form>
+      <div className="card max-w-md">
+        <div className="card-body">
+          <p className="mb-5 text-2xl">Register With RefugeeOne Job Search Portal</p>
+          <form onSubmit={handleSubmit(signUp)}>
+            <label className="label" htmlFor="name">
+              <span className="label-text">Full Name</span>
+            </label>
+            <input
+              {...register("name", { required: true })}
+              type="text"
+              className="input w-full max-w-xs input-bordered"
+            />
+            <label className="label" htmlFor="email">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              {...register("email", { required: true })}
+              type="email"
+              className="input w-full max-w-xs input-bordered mb-4"
+            />
+            <label className="label" htmlFor="compnay">
+              <span className="label-text">Company Name</span>
+            </label>
+            <input
+              {...register("company")}
+              type="text"
+              className="input w-full max-w-xs input-bordered mb-4"
+              disabled
+            />
+            <label className="label" htmlFor="phone">
+              <span className="label-text">Phone Number</span>
+            </label>
+            <input
+              {...register("phone", { required: true })}
+              type="number"
+              className="input w-full max-w-xs input-bordered mb-4"
+            />
+            <label className="label" htmlFor="password">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              {...register("password", { required: true })}
+              type="password"
+              className="input w-full max-w-xs input-bordered mb-4"
+            />
+            <label className="label" htmlFor="role">
+              <span className="label-text">Role</span>
+            </label>
+            <input
+              {...register("role", { value: "employer" })}
+              type="text"
+              className="input w-full max-w-xs input-bordered mb-4"
+              disabled
+            />
+            <button
+              type="submit"
+              className={`btn btn-primary ${signUpLoading ? "loading" : ""}`}
+              disabled={signUpLoading}
+            >
+              {signUpLoading ? "Loading" : "Sign Up"}
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
