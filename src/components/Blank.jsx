d60b19ff6aa0c278284d0cfbbef4abf06ccd86c5
makersmelx <makersmelx@gmail.com>
2023-03-31 02:38:16 -0400

    refact: job view

--- src/components/Blank.jsx
+++ src/components/Blank.jsx
@@ -1,7 +1,7 @@
-export default function Blank() {
+export default function Blank({ children }) {
   return (
-    <div className="w-full h-full">
-      <div className="hero-content text-center">I am a blank page~</div>
+    <div className="w-full h-full flex items-center justify-center">
+      <div className="hero-content text-center">{children}</div>
     </div>
   );
 }
