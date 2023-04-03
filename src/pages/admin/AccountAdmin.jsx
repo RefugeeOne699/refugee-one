e654274f0d0b5bbe011e336b0587b8d280a5c931
Ken Debacker <kcdebacker@gmail.com>
2023-04-02 21:24:25 -0400

    modify job.jsx for users

---
+++ src/pages/admin/AccountAdmin.jsx
@@ -0,0 +1,63 @@
+import { useRequest } from "ahooks";
+import { useEffect, useMemo } from "react";
+import { toast } from "react-hot-toast";
+import { NavLink, useParams } from "react-router-dom";
+
+import Spin from "@/components/Spin";
+import { useJob } from "@/models";
+
+const menu = [
+  { url: "employer", name: "Employer Accounts" },
+  { url: "refugee", name: "Refugee Accounts" },
+  { url: "admin", name: "Admin Accounts" },
+];
+
+export default function JobsAdmin() {
+  const { tabUrl } = useParams();
+  const { listJobs } = useJob();
+  const { data, run, loading } = useRequest(async () => listJobs(), {
+    manual: true,
+    onError: () => {
+      toast.error("Failed to fetch job list");
+    },
+  });
+  useEffect(() => {
+    (async () => {
+      await run();
+    })();
+  }, []);
+  const tabs = useMemo(() => {
+    return (
+      <div className="tabs tabs-boxed">
+        {menu.map((tab) => (
+          <NavLink
+            to={`/admin/jobs/${tab.url}`}
+            className={({ isActive }) => `tab ${isActive ? "tab-active" : null}`}
+            key={tab.name}
+          >
+            <div className="flex flex-row">
+              {/*  todo: do what figma designs */}
+              {tab.name}
+              {"---"}
+              {/*  todo: update the count */}
+              {loading ? <Spin /> : data ? data.length : 0}
+            </div>
+          </NavLink>
+        ))}
+      </div>
+    );
+  }, [tabUrl, data, loading]);
+  const content = useMemo(() => {
+    if (loading) {
+      return <Spin className="h-8 w-8" />;
+    }
+    //todo: display and filter
+    return data ? JSON.stringify(data) : "no data";
+  }, [data, loading, tabUrl]);
+  return (
+    <div className="flex flex-col">
+      {tabs}
+      {content}
+    </div>
+  );
+}
