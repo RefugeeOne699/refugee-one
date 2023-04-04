c654063e196b33bd5a40950c4592e5a274921f96
Ken Debacker <kcdebacker@gmail.com>
2023-04-03 21:28:17 -0400

    forgot to remove useUser

--- src/models/index.js
+++ src/models/index.js
@@ -4,11 +4,9 @@ import { AdminContext } from "./admin";
 import { AuthContext } from "./auth";
 import { JobContext } from "./job";
 import { JobSaveContext } from "./jobSave";
-import { UserContext } from "./user";
 
 const useAuth = () => useContext(AuthContext);
 const useJob = () => useContext(JobContext);
 const useAdmin = () => useContext(AdminContext);
 const useJobSave = () => useContext(JobSaveContext);
-const useUser = () => useContext(UserContext);
-export { useAdmin, useAuth, useJob, useJobSave, useUser };
+export { useAdmin, useAuth, useJob, useJobSave };
