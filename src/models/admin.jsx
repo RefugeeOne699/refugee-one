0611ecc9e7a5f8168f88d8ebe39714c18f1649fd
Ken Debacker <kcdebacker@gmail.com>
2023-04-05 23:53:10 -0400

    mike's code suggestion for user.status

--- src/models/admin.jsx
+++ src/models/admin.jsx
@@ -64,7 +64,7 @@ const AdminContextProvider = ({ children }) => {
         phone: user.phone,
         role: user.role,
         // to deal with existing accounts who don't have status field
-        status: user.status === undefined ? USER_STATUS.PENDING : user.status,
+        status: user.status || USER_STATUS.PENDING,
       };
     });
     return await Promise.all(userList);
