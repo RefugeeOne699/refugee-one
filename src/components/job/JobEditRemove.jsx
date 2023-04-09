a8dd5896449b71d0d29b458b4ac194818dc91c4e
Mingyu Li <lmyannie60@163.com>
2023-04-08 21:57:13 -0400

    admin and employer dashboard

--- src/components/job/JobEditRemove.jsx
+++ src/components/job/JobEditRemove.jsx
@@ -1,17 +1,21 @@
-export default function JobEditRemove({ status }) {
+import { useNavigate } from "react-router-dom";
+
+export default function JobEditRemove({ jobId, status }) {
+  const navigate = useNavigate();
   return (
     <>
       <div className="items-center text-center w-3/4">
         <div className="w-full flex flex-row justify-center">
           <button
-            className={`btn btn-primary btn-outline ml-5 ${
-              status === "pending" || status === "approved" ? "" : "hidden"
-            }`}
+            className={`btn btn-primary ${
+              status === "approved" ? "btn-outline" : ""
+            } ml-5 `}
+            onClick={() => navigate("../../../../addJob", { state: { jobId: jobId } })}
           >
             Edit job
           </button>
           <button
-            className={`btn btn-primary ml-5 ${status === "pending" ? "hidden" : ""}`}
+            className={`btn btn-primary ml-5 ${status === "approved" ? "" : "hidden"}`}
           >
             Remove Job Post
           </button>
