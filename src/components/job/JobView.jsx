17f3880638da339025ebc4e076d869486db5c414
10RE <lorenlin@umich.edu>
2023-04-13 10:33:17 -0400

    fix distance type

--- src/components/job/JobView.jsx
+++ src/components/job/JobView.jsx
@@ -37,7 +37,7 @@ export default function JobView() {
             jobCoordinate.latitude,
             jobCoordinate.longitude
           );
-          data.distance = distance;
+          data.distance = distance.toFixed(1).toString();
         } else {
           data.distance = null;
         }
@@ -168,7 +168,7 @@ export default function JobView() {
                 </a>
                 <p>
                   {data.distance
-                    ? data.distance.toFixed(1) + " miles from you"
+                    ? data.distance + " miles from you"
                     : "Distance not applied"}
                 </p>
               </div>
